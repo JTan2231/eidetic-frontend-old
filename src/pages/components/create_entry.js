66ec3289a06a739554ab06c0d15eed0e1a15dddc
joey <j.tan2231@gmail.com>
2023-01-29 00:32:18 -0500

    functioning interface; entry-level navigation

---
+++ src/pages/components/create_entry.js
@@ -0,0 +1,62 @@
+import * as config from '../../util/config.js';
+import { getCookie } from '../../util/cookie.js';
+import { useRef, useState } from 'react';
+
+import '../../styles/entries.css';
+import '../../styles/login.css';
+
+
+export const CreateEntry = () => {
+    const entryInput = useRef();
+    const titleInput = useRef();
+
+    const [height, setHeight] = useState('0');
+
+    const createEntryAttempt = () => {
+        fetch(`${config.API_ROOT}create-entry/`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title: titleInput.current.value,
+                content: entryInput.current.value,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(res => res.json()).then(res => {
+            window.location.reload();
+        });
+    };
+
+    const handleFocus = () => {
+        setHeight('2em');
+    };
+
+    const handleBlur = () => {
+        const inp = entryInput.current;
+        if (inp && inp.value.length > 0) {
+            setHeight('2em');
+        }
+        else {
+            setHeight('0');
+            titleInput.current.value = '';
+        }
+    };
+
+    const groupStyle = {
+        height: height,
+        opacity: height === '0' ? '0' : '1',
+        padding: height === '0' ? '0 1em' : '1px 1em 1em',
+    };
+
+    return (
+        <>
+            <textarea ref={entryInput} className="newEntry" placeholder="Create a new entry" onFocus={handleFocus} onBlur={handleBlur} />
+            <div style={groupStyle} className="saveEntryGroup">
+                <input ref={titleInput} className="newEntryTitleInput" type="text" placeholder="Title" />
+                <div className="saveEntryButton" onClick={createEntryAttempt}>
+                    <span>Save</span>
+                </div>
+            </div>
+        </>
+    );
+};
---
+++ src/pages/components/entry.js
@@ -0,0 +1,69 @@
+import * as config from '../../util/config';
+import { useState, useEffect } from 'react';
+import { splitTimestamp } from '../../util/formatting';
+
+import '../../styles/entries.css';
+
+export const Entry = (props) => {
+    const [related, setRelated] = useState([]);
+    const [id, setID] = useState(-1);
+    const [textClasses, setTextClasses] = useState('entryContent textClipped');
+    const [showText, setShowText] = useState('Show more');
+
+    useEffect(() => {
+        setID(props.id);
+    }, []);
+
+    const fetchRelated = () => {
+        fetch(`${config.API_ROOT}entry-links/?entry_id=${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(res => res.json()).then(res => {
+            const entries = res.entries;
+            setRelated(entries.map(r => (
+                <Entry id={r.entry_id} title={r.title} timestamp={r.timestamp} content={r.content} />
+            )));
+        });
+    };
+
+    const collapse = () => {
+        setRelated([]);
+    };
+
+    const showMore = () => {
+        setTextClasses('entryContent');
+        setShowText('Show less');
+    };
+
+    const showLess = () => {
+        setTextClasses('entryContent textClipped');
+        setShowText('Show more');
+    };
+
+    const entryJSONToEntryOutline = (entryJSON) => {
+        return (
+            <div className="entryOutline">
+                <div className="entry">
+                    <div className="entryTitle">{entryJSON.title}</div>
+                    <div className="entryTimestamp">{splitTimestamp(entryJSON.timestamp)}</div>
+                    <div className={textClasses}>{entryJSON.content}</div>
+                    <div className="entryActionBar">
+                        <span className="entryAction" onClick={showText === 'Show more' ? showMore : showLess}>{showText}</span>
+                        <span className="entryAction" onClick={related.length > 0 ? collapse : fetchRelated}>
+                            {related.length > 0 ? 'Collapse' : 'See related'}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="entryBlock">
+            {entryJSONToEntryOutline(props)}
+            {related}
+        </div>
+    );
+}
